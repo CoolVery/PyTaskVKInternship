6d29b17ab6622c58c9104867829f4ce434243453
CoolVery <georgekuklevwork@gmail.com>
2024-05-29 23:31:53 +0300

    Recreated the project

---
+++ main.py
@@ -0,0 +1,40 @@
+import secondHM
+import thirdHM
+import fourthHM
+import fifthHM
+import boundaryControlFirst
+
+if __name__ == "__main__":
+    #----------------------------------
+
+    #print(secondHM.CorrectText("letter"))
+    #print(secondHM.CorrectText("ppa"))
+
+    #----------------------------------
+
+    #----------------------------------
+
+    #textFirst, textSecond, textThird = int(input()), float(input()), int(input())
+    #thirdHM.CorrectOutputString(textFirst, textSecond, textThird)
+
+    #text = input()
+
+    #thirdHM.ChangeAndDeleteSymbolText(text)
+    #----------------------------------
+
+    #----------------------------------
+
+    #fourthHM.AverageValueOfCharactersInAWord(input())
+    #fourthHM.ReverseMaxValueInLists(int(input()))
+
+    # ----------------------------------
+
+    #fifthHM.SetCharInStroce(input())
+    #fifthHM.MaxCountWordInText(input())
+
+    # ----------------------------------
+
+    #boundaryControlFirst.FindTheRightWordsInTheText(input())
+
+    # ----------------------------------
+    print()
